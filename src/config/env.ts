3b9e1cc13c7f5453ecac94dd00ea41c575088758
Holayn <kwwong1597@gmail.com>
2026-01-01 16:14:41 -0500

    Silence dotenv messages.

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,6 +1,6 @@
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ quiet: true });
 
 export const PORT = process.env.PORT || 3000;
 export const NODE_ENV = process.env.NODE_ENV || 'development';
