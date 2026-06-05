89c7910f38d1760d2f7bbd7858ac10fee40a944b
Holayn <kwwong1597@gmail.com>
2026-06-04 20:53:53 -0400

    Move NOTIFY_SERVICE_URL

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,5 +2,13 @@ import dotenv from 'dotenv';
 
 dotenv.config({ quiet: true });
 
+if (!process.env.NOTIFY_SERVICE_URL && process.env.NODE_ENV !== 'development') {
+  throw new Error('Notify service URL is not defined.');
+}
+
+export const NOTIFY_SERVICE_URL = process.env.NOTIFY_SERVICE_URL;
+export const NOTIFY_SERVICE_USER = process.env.NOTIFY_SERVICE_USER;
 export const PORT = process.env.PORT || 3000;
 export const NODE_ENV = process.env.NODE_ENV || 'development';
+
+export const isDevelopment = NODE_ENV === 'development';
--- src/services/notify.ts
+++ src/services/notify.ts
@@ -1,8 +1,4 @@
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-const { NOTIFY_SERVICE_URL, NOTIFY_SERVICE_USER } = process.env;
+import { NOTIFY_SERVICE_URL, NOTIFY_SERVICE_USER, isDevelopment } from '../config/env.js';
 
 type NotifyData = {
   [key: string]: string;
@@ -13,6 +9,11 @@ export async function notify(
   message: string,
   data: NotifyData = {} as NotifyData,
 ) {
+  if (isDevelopment) {
+    console.log('Notification:', message, data);
+    return;
+  }
+
   if (!NOTIFY_SERVICE_URL) {
     throw new Error('Notify service URL is not defined.');
   }
